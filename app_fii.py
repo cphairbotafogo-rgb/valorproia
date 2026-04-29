0e17e8db9371fb5e773a7cbed2749f2b1c1ee7a2
cphairbotafogo-rgb <cphairbotafogo@conceptprimehair.com>
2026-04-29 01:51:12 -0300

    Update app_fii.py

--- app_fii.py
+++ app_fii.py
@@ -28,12 +28,7 @@ from motor import *
 # 1. Primeiro você define o endereço (A "variável")
 URL_LOGO_OFICIAL = "https://dcvbigplgruvaojmutth.supabase.co/storage/v1/object/public/logos/ChatGPT%20Image%2028%20de%20abr.%20de%202026,%2022_55_53.png"
 
-# 2. Depois você usa ela na configuração
-st.set_page_config(
-    page_title="ValorPro IA", 
-    page_icon=URL_LOGO_OFICIAL, 
-    layout="wide", 
-    initial_sidebar_state="expanded")
+
 
 try:
     from supabase import create_client, Client
