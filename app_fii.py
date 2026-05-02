ab8472fa64d0a8faa3fda5105c32dec32853e9eb
cphairbotafogo-rgb <cphairbotafogo@conceptprimehair.com>
2026-05-01 22:04:34 -0300

    Update app_fii.py

--- app_fii.py
+++ app_fii.py
@@ -31,7 +31,7 @@ URL_LOGO_OFICIAL = "https://dcvbigplgruvaojmutth.supabase.co/storage/v1/object/p
 # 2. Depois você usa ela na configuração
 st.set_page_config(
     page_title="ValorPró IA",
-    page_icon="https://cdn-icons-png.flaticon.com/512/2454/2454282.png", 
+    page_icon="https://dcvbigplgruvaojmutth.supabase.co/storage/v1/object/public/logos/ChatGPT%20Image%2028%20de%20abr.%20de%202026,%2022_55_53.png", 
     layout="wide",
     initial_sidebar_state="expanded"
 )
